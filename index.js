833a4a3ef6658c7e1a7cdbd708137d00d2674d64
anjalidhanjode28 <anjalidhanjode28@gmail.com>
2023-01-04 19:37:11 +0530

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
+server.use(middlewares);
+server.use(router);
+
+
+server.listen(port, () => {
+  console.log('JSON Server is running', port);
+});
